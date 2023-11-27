65650d1ec35d5a4fe9773de768f62805f4e2ed5b
ahmedzehry55 <ahmedalhlwany2111@gmail.com>
2023-11-27 10:41:30 +0200

    'header'

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,32 +8,31 @@ const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <nav className="app__navbar">
-       <div className="app__navbar-login">
-        <a href="#login" className="p__opensans">تسجيل الدخول \ مستخدم جديد </a>
-        <div />
-      </div>
-
+      
+      {/* menu section */}
       <ul className="app__navbar-links">
         <li className="p__opensans"><a href="#home">الرئيسية</a></li>
         <li className="p__opensans"><a href="#about">عنا</a></li>
         <li className="p__opensans"><a href="#menu">الاصناف</a></li>
         <li className="p__opensans"><a href="#contact">اتصل بنا</a></li>
       </ul>
-     
+    
+     {/* logo section  */}
       <div className="app__navbar-logo">
-        <img src={images.babaAbdo} alt="app__logo" />
+        <img src={images.babaAbdo} alt="baba abdo بابا عبدو" />
       </div>
+
+    {/* smallscreen menu section  */}
       <div className="app__navbar-smallscreen">
         <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => setToggleMenu(true)} />
         {toggleMenu && (
           <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
-            <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
+            <MdOutlineRestaurantMenu className="overlay__close" onClick={() => setToggleMenu(false)} />
             <ul className="app__navbar-smallscreen_links">
-              <li><a href="#home" onClick={() => setToggleMenu(false)}>Home</a></li>
-              <li><a href="#about" onClick={() => setToggleMenu(false)}>About</a></li>
-              <li><a href="#menu" onClick={() => setToggleMenu(false)}>Menu</a></li>
-              <li><a href="#awards" onClick={() => setToggleMenu(false)}>Awards</a></li>
-              <li><a href="#contact" onClick={() => setToggleMenu(false)}>Contact</a></li>
+              <li><a href="#home" onClick={() => setToggleMenu(false)}>الرئيسية</a></li>
+              <li><a href="#about" onClick={() => setToggleMenu(false)}>عنا</a></li>
+              <li><a href="#menu" onClick={() => setToggleMenu(false)}>الاصناف</a></li>
+              <li><a href="#contact" onClick={() => setToggleMenu(false)}>اتصل بنا</a></li>
             </ul>
           </div>
         )}
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header_img" />
+    </div>
+
+    <div className="app__wrapper_info">
+      <SubHeading title="من أول قطمة هتاخدك لعالم تاني" />
+      <h1 className="app__header-h1">أحنا بتووع الأكل الأصلي</h1>
+      <p className="p__opensans" style={{ margin: '2rem 0' }}>جعان وكرشك مش مبسوط سد جوعك بالطعم الأصلى المظبوط 
+      بابا عبده عنده الطعم الاصلى اللى بتدور عليه 
+      الطعم اللى يخلى كرشك فى حالة انبساط 
+      وكل اللى بتتمناه عندنا وفرناه 
+      علشان خاطر الكرش يبقى فريش 
+      دلع كرشك مع  البابا عبده  </p>
+      <button type="button" className="custom__button">المزيد</button>
+    </div>
+
   </div>
 );
 
